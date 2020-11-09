ea7c10c03b9102b08750c373804d74af0a10d30c
Noal Brown <noalbrown1@gmail.com>
2020-11-09 15:41:31 -0600

    finish

---
+++ app.js
@@ -0,0 +1,21 @@
+// const sayHello = function () {
+//   console.log('Hello')
+// }
+
+// const sayHello = () => console.log('Hello')
+
+// const sayHello = () => {
+//   console.log('Hello')
+// }
+
+// const sayHello = () => ({msg: 'Hello'})
+
+// const sayHello = (firstName, lastName) => console.log(`Hello ${firstName}${lastName}`)
+
+// sayHello('Noal', 'Brown');
+
+const users = ['Bob', 'John', 'William']
+
+const nameLengths = users.map(name => name.length)
+
+console.log(nameLengths)
