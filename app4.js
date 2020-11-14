b7a43f12fa5d7c19ae6635e40a48e20c8e61a5e9
Noal Brown <noalbrown1@gmail.com>
2020-11-14 12:16:46 -0600

    finish

--- app4.js
+++ app4.js
@@ -45,9 +45,51 @@ map1.set(key3, 'Value of key3')
 // console.log(keyValArr)
 
 // Create an array of values
-const valArr = Array.from(map1.values())
-console.log(valArr)
+// const valArr = Array.from(map1.values())
+// console.log(valArr)
 
-// Create an array of keys
-const keyArr = Array.from(map1.keys())
-console.log(keyArr)
+// // Create an array of keys
+// const keyArr = Array.from(map1.keys())
+// console.log(keyArr)
+
+
+// Sets store a unique value of any type
+const set1 = new Set()
+
+// Add values to set
+set1.add(100)
+set1.add('a string')
+set1.add({ name: 'John' })
+set1.add(true)
+
+// const set2 = new Set([1, true, 'string'])
+
+//  console.log(set1)
+
+//  Get count
+// console.log(set1.size)
+
+// Check for values
+// console.log(set1.has(100))
+
+// delete from set
+set1.delete(100)
+
+// console.log(set1)
+
+// iterate through sets
+// for...of
+// for(let item of set1){
+//   console.log(item)
+// }
+
+// // for each loop
+// set1.forEach((value) => {
+//   console.log(value)
+// })
+
+// convert set to array
+const setArr = Array.from(set1)
+console.log(setArr)
+
+
