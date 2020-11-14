f055daa1d9fab24ad293f7d408982ca03e6bc73a
Noal Brown <noalbrown1@gmail.com>
2020-11-14 11:16:02 -0600

    practice

---
+++ app4.js
@@ -0,0 +1,53 @@
+// Maps = key value pairs and can use any type
+
+const map1 = new Map();
+
+// Set keys
+const key1 = 'some string',
+  key2 = {},
+  key3 = function () { }
+
+// Set map values by key
+map1.set(key1, 'Value of key1')
+map1.set(key2, 'Value of key2')
+map1.set(key3, 'Value of key3')
+
+// Get values by key
+// console.log(map1.get(key1))
+
+// Count values in map
+// console.log(map1.size)
+
+// Iterate through maps
+// Loop using for...of to get keys and values
+// for (let [key, value] of map1) {
+//   // console.log(`${key} = ${value}`)
+// }
+
+// // Iterate keys only
+// for (let key of map1.keys) {
+//   console.log(key)
+// }
+
+// // Iterate values only
+// for (let key of map1.values()) {
+//   console.log(values)
+// }
+
+// Loop with forEach
+// map1.forEach(function (value, key) {
+//  console.log(`${key} = ${value}`)
+// })
+
+// Convert to arrays
+// Create an array of key value pairs
+// const keyValArr = Array.from(map1)
+// console.log(keyValArr)
+
+// Create an array of values
+const valArr = Array.from(map1.values())
+console.log(valArr)
+
+// Create an array of keys
+const keyArr = Array.from(map1.keys())
+console.log(keyArr)
